33cff61416c4f27b28192b935aef14823e97c0cf
namkha1032 <namkha1032@gmail.com>
2022-12-06 15:18:26 +0700

    create sample

--- components/foot.php
+++ components/foot.php
@@ -1,6 +1,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.1.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/app.js"></script>
     <script>
         $(document).ready(function() {
             $('.datatable').DataTable();
